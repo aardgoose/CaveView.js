41471a887604d706e8617255a4cc83881ccd1b46
aardgoose <angus.sawyer@gmail.com>
2016-08-27 17:48:00 +0100

    further EntranceFarPointer work visibility management now supported. (misc changes to use traverseReverse now in own fork of Three.js

--- src/js/viewer/EntranceFarPointer.js
+++ src/js/viewer/EntranceFarPointer.js
@@ -1,29 +1,38 @@
 
-import { getEnvironmentValue } from '../core/constants.js';
+import { getEnvironmentValue, FEATURE_ENTRANCES } from '../core/constants.js';
 
 import {
 	Vector3, Color,
-	Geometry,
+	BufferGeometry,
+	DirectGeometry,
 	TextureLoader,
+	MultiMaterial,
 	PointsMaterial,
 	Points,
-	Object3D
+	Object3D,
 } from '../../../../three.js/src/Three.js';
 
 var farPointers = null;
 
 function FarPointers ( survey ) {
 
-	var geometry = new Geometry();
+	this.directGeometry = new DirectGeometry();
 	var loader = new TextureLoader();
 
-	var texture = loader.load( getEnvironmentValue( "cvDirectory", "" ) + "CaveView/images/marker-yellow.png" );
+	var yellowTexture = loader.load( getEnvironmentValue( "cvDirectory", "" ) + "CaveView/images/marker-yellow.png" );
+	var cyanTexture   = loader.load( getEnvironmentValue( "cvDirectory", "" ) + "CaveView/images/marker-cyan.png" );
 
-	var material = new PointsMaterial( { size: 10, map: texture, transparent : true, sizeAttenuation: false } );
+	var nullMaterial   = new PointsMaterial( { visible: false } );
+	var yellowMaterial = new PointsMaterial( { size: 10, map: yellowTexture, transparent : true, sizeAttenuation: false } );
+	var cyanMaterial   = new PointsMaterial( { size: 10, map: cyanTexture, transparent : true, sizeAttenuation: false } );
 
-	Points.call( this, geometry, material );
+	var material = new MultiMaterial( [ nullMaterial, yellowMaterial ] );
+
+	Points.call( this, new BufferGeometry(), material );
 
 	this.type = "CV.FarPointers";
+
+	this.layers.set( FEATURE_ENTRANCES );
 
 	survey.add( this );
 
@@ -42,23 +51,47 @@ function FarPointers ( survey ) {
 
 }
 
+// FIXME - move to BufferGeometry for simplicity
+
 FarPointers.prototype = Object.create( Points.prototype );
 
 FarPointers.prototype.constructor = FarPointers;
 
 FarPointers.prototype.addPointer = function ( position ) {
 
-	var geometry = this.geometry;
+	var geometry = this.directGeometry;
+	var index = geometry.vertices.length;
 
 	geometry.vertices.push( position );
-	geometry.colors.push( new Color( 0xff00ff ) );
+	geometry.groups.push( { start: index, count: 1, materialIndex: 1 } );
 
-	geometry.verticesNeedUpdate = true;
-	geometry.colorsNeedUpdate = true;
+//	geometry.verticesNeedUpdate = true;
+
+
+	this.geometry.fromDirectGeometry( geometry );
+
+	return index;
+
+}
+
+FarPointers.prototype.setMaterialIndex = function ( index, materialIndex ) {
+
+	var geometry = this.directGeometry;
+
+	geometry.groups[index].materialIndex = materialIndex;
+	this.geometry = new BufferGeometry().fromDirectGeometry( geometry );
+
+}
+
+FarPointers.prototype.getMaterialIndex = function ( index ) {
+
+	return  this.directGeometry.groups[index].materialIndex;
 
 }
 
 function EntranceFarPointer ( survey, position ) {
+
+	var self = this;
 
 	if ( farPointers === null ) {
 
@@ -66,11 +99,36 @@ function EntranceFarPointer ( survey, position ) {
 
 	}
 
-	farPointers.addPointer( position );
-
 	Object3D.call( this );
 
+	this.index = farPointers.addPointer( position );
 	this.type = "CV.EntranceFarPointer";
+
+	Object.defineProperty( this, "visible", {
+		writeable: true,
+		get: function () { _getVisibility(); },
+		set: function ( x ) { _setVisibility( x ); }
+	} );
+
+	var fp = farPointers;
+
+	function _getVisibility () {
+
+		fp.getMaterialIndex( self.index );
+
+	}
+
+	function _setVisibility ( visible ) {
+
+		var newIndex = ( visible === false ) ? 0 : 1;
+
+		if ( newIndex !== fp.getMaterialIndex( self.index ) ) {
+
+			fp.setMaterialIndex( self.index, newIndex );
+
+		}
+
+	}
 
 }
 

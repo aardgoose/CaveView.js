54fe88b389a5ba0f13840ba1dd1124f3bcf4450d
aardgoose <angus.sawyer@gmail.com>
2016-09-27 10:03:39 +0100

    export CaveLoader

--- src/js/CV.js
+++ src/js/CV.js
@@ -4,4 +4,4 @@ export * from './core/constants.js';
 export { Viewer } from './viewer/Viewer.js';
 export { UI } from './ui/UI.js';
 export { HUD } from './hud/HUD.js';
-//export * from './core/Object3D.js';
+export { CaveLoader } from './loaders/CaveLoader.js';

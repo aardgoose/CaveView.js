0ef3b253764b22e232282202946088259958065e
aardgoose <angus.sawyer@gmail.com>
2016-11-10 10:54:22 +0000

    use new material in DirectionGlobe experiment

--- src/js/analysis/DirectionGlobe.js
+++ src/js/analysis/DirectionGlobe.js
@@ -1,15 +1,21 @@
 
 import  {
-	Points, PointsMaterial, VertexColors, SphereBufferGeometry, MeshBasicMaterial, Mesh, Geometry, Vector3
+	Points, VertexColors, SphereBufferGeometry, MeshBasicMaterial, Mesh, Geometry, Vector3, BufferGeometry, Float32BufferAttribute,
 } from '../../../../three.js/src/Three.js'; 
+
+import  {
+	ExtendedPointsMaterial,
+} from '../materials/ExtendedPointsMaterial.js'; 
 
 import { ColourCache } from '../core/ColourCache.js';
 
 function DirectionGlobe ( survey ) {
 
 	var geometry = new Geometry();
+	var bufferGeometry = new BufferGeometry();
 
-	Points.call( this, geometry, new PointsMaterial( { size: 1.0, opacity: 0.5, transparent: true,  vertexColors: VertexColors  } ) );
+	var material = new ExtendedPointsMaterial( { size: 1.0, opacity: 0.5, transparent: true,  vertexColors: VertexColors  } );
+	Points.call( this, bufferGeometry, material );
 
 	this.type = "DirectionGlobe";
 
@@ -30,15 +36,22 @@ function DirectionGlobe ( survey ) {
 
 	var l = vertices.length;
 
+	var pSize = [];
+
 	for ( var i = 0; i < l; i += 2 ) {
 
 		legVector = new Vector3().subVectors( vertices[ i ], vertices[ i + 1 ] );
 
 		rLength = ( legVector.length() - stats.minLegLength ) / stats.legLengthRange;
 
+		var c = Math.max( 0, 2 * ( 1 + Math.log(  rLength * 10 ) * Math.LOG10E ) );
+	
+		pSize.push( c );
+		pSize.push( c );
+
 		color = colours[ Math.max( 0, Math.floor( bias * ( 1 + Math.log(  rLength * 10 ) * Math.LOG10E ) / 2 ) ) ];
 
-		legVector.setLength( 40 );
+		legVector.setLength( 40 + rLength );
 
 		geometry.vertices.push( legVector.clone().negate() );
 		geometry.vertices.push( legVector );
@@ -47,6 +60,13 @@ function DirectionGlobe ( survey ) {
 		geometry.colors.push( color );
 
 	}
+
+	var positions = new Float32BufferAttribute( geometry.vertices.length * 3, 3 );
+	var colors = new Float32BufferAttribute( geometry.colors.length * 3, 3 );
+
+	bufferGeometry.addAttribute( "pSize", new Float32BufferAttribute( pSize, 1 ) );
+	bufferGeometry.addAttribute( 'position', positions.copyVector3sArray( geometry.vertices ) );
+	bufferGeometry.addAttribute( 'color', colors.copyColorsArray( geometry.colors ) );
 
 	survey.addEventListener( "removed", _onSurveyRemoved );
 

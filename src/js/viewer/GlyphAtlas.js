1cac2cf04df95cef91d8d3a0ed03f93b9ea67ccc
aardgoose <angus.sawyer@gmail.com>
2017-05-25 14:21:58 +0100

    add Gl;yphAtlas (texture and glyph mappings/metrics)

---
+++ src/js/viewer/GlyphAtlas.js
@@ -0,0 +1,108 @@
+import { CanvasTexture } from '../../../../three.js/src/Three';
+
+import { GlyphMaterial } from '../materials/GlyphMaterial';
+
+function GlyphAtlas () {
+
+	var atlasSize = 512;
+	var cellSize = 32;
+
+	var canvas = document.createElement( 'canvas' );
+	var map = {};
+
+	if ( ! canvas ) console.error( 'creating canvas for glyph atlas failed' );
+
+	canvas.width  = atlasSize;
+	canvas.height = atlasSize;
+
+//	document.body.appendChild( canvas ); // FIXME debug code
+
+	var ctx = canvas.getContext( '2d' );
+
+	if ( ! ctx ) console.error( 'cannot obtain 2D canvas' );
+
+	// set background
+
+	ctx.fillStyle = 'rgba( 255, 0, 0, 1 )';
+	ctx.fillRect( 0, 0, atlasSize, atlasSize );
+
+	// populate with glyphs
+
+	var glyphs = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-_';
+
+	var divisions = atlasSize / cellSize;
+
+	var glyphCount = glyphs.length;
+
+	if ( glyphCount > divisions * divisions ) {
+
+		console.error( 'too many glyphs for atlas' );
+		return;
+
+	}
+
+	var glyph;
+	var fontSize = 20;
+
+	ctx.textAlign = 'left';
+	ctx.font = 'normal ' + fontSize + 'px helvetica,sans-serif';
+	ctx.fillStyle = '#ffffff';
+
+	var row, column;
+
+	for ( var i = 0; i < glyphCount; i++ ) {
+
+		glyph = glyphs.charAt( i );
+
+		var glyphWidth = ctx.measureText( glyph ).width / cellSize;
+
+		row = Math.floor( i / divisions ) + 1;
+		column = i % divisions;
+
+		map[ glyph ] = { row: ( divisions - row ) / divisions, column: column / divisions, width: glyphWidth };
+
+		ctx.fillText( glyph, cellSize * column, cellSize * row - 6 );
+
+	}
+
+	this.texture = new CanvasTexture( canvas );
+	this.map = map;
+	this.cellSize = cellSize;
+	this.atlasSize = atlasSize;
+	this.material = undefined;
+
+}
+
+GlyphAtlas.prototype.getMaterial = function () {
+
+	if ( this.material === undefined ) {
+
+		this.material = new GlyphMaterial( this.texture, this.cellSize / this.atlasSize );
+
+	}
+
+	return this.material;
+
+};
+
+GlyphAtlas.prototype.getGlyph = function ( glyph ) {
+
+	var glyphData = this.map[ glyph ];
+
+	if ( glyphData === undefined ) {
+
+		console.warn( 'unavailable glyph' );
+		glyphData = {}; // FIXME add placeholder to atlas.
+
+	}
+
+	return glyphData;
+
+};
+
+GlyphAtlas.prototype.constructor = GlyphAtlas;
+
+
+export { GlyphAtlas };
+
+// EOF

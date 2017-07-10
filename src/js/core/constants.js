b3235c274b7b6038dd4c395c657705ba551024fc
aardgoose <angus.sawyer@gmail.com>
2017-07-10 21:16:34 +0100

    remove build minimising hack

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -1,5 +1,5 @@
 
-import { Vector3 }  from '../../../../three.js/src/Three-workers';
+import { Vector3 }  from '../../../../three.js/src/math/Vector3';
 
 export var MATERIAL_LINE       = 1;
 export var MATERIAL_SURFACE    = 2;

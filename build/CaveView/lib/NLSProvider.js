270ae63f4d77d9ef16dad965f0453f8fdfa0d066
aardgoose <angus.sawyer@gmail.com>
2017-07-27 19:50:41 +0100

    add missing files  (and corrected .gitignore)

--- build/CaveView/lib/NLSProvider.js
+++ build/CaveView/lib/NLSProvider.js
@@ -4,6 +4,9 @@ function NLSProvider () {
 
 }
 
+NLSProvider.prototype.minZoom = 12;
+NLSProvider.prototype.maxZoom = 14;
+
 NLSProvider.prototype.getUrl = function ( x, y, z ) {
 
 	return NLSTileUrlOS( x, y, z );

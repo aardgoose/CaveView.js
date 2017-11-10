ff664392c38d1f6f3968b766ff96a5374110b381
aardgoose <angus.sawyer@gmail.com>
2017-11-10 11:15:52 +0000

    update scripts to make terrain

--- tools/makeTiles.js
+++ tools/makeTiles.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var halfMapExtent = 6378137 * Math.PI; // from EPSG:3875 definition
 var mapSet = 'UK';
@@ -9,22 +9,23 @@ function tileArea( x, y, z, maxZoom ) {
 	var tileWidth = halfMapExtent / Math.pow( 2, z - 1 );
 	var resolution = tileWidth / 128; // note: tile area extended by resolution / 2 all round givving 256 sample row & columns
 	var offset = resolution / 2;
+	var outFile;
 
 	var n, s, e, w;
 	var cmd;
 
 	n = halfMapExtent - y * tileWidth + offset;
-	s = halfMapExtent - ( y + 1 ) * tileWidth - offset
+	s = halfMapExtent - ( y + 1 ) * tileWidth - offset;
 
 	w = - halfMapExtent + x * tileWidth - offset;
-	e = - halfMapExtent + ( x + 1 ) * tileWidth + offset
+	e = - halfMapExtent + ( x + 1 ) * tileWidth + offset;
 
 	if ( z > 8 ) {
 
 		cmd =  'g.region n=' + n + ' s=' + s + ' w=' +  w + ' e=' + e + ' nsres=' + resolution + ' ewres=' + resolution;
 		console.log( cmd );
 
-		outFile = 'dtm\\' + z + '\\DTM-' + x + "-" + y + '.bin';
+		outFile = 'dtm\\' + z + '\\DTM-' + x + '-' + y + '.bin';
 
 		cmd = 'r.out.bin -b bytes=2 input=DTM' + z + 'X@' + mapSet +  ' output=' + outFile;
 		console.log( cmd );
@@ -49,32 +50,29 @@ function tileArea( x, y, z, maxZoom ) {
 
 // EPSG:3875 "Web Mercator" tile range
 
-var mapSet = 'Mulu';
-var sourceRaster = 'mulu1';
-
 var tileSet = {
-	title: 'Mulu',
-	dtmMaxZoom: 13,
+	title: 'OFD',
+	dtmMaxZoom: 17,
 	maxZoom: 18,
-	minZoom: 10,
+	minZoom: 13,
 	divisions: 128,
 	directory: '',
-	subdirectory: 'mulu',
+	subdirectory: 'OFD',
 	dtmScale: 64,
-	minX: 838,
-	maxX: 839,
-	minY: 499,
-	maxY: 500
+	minX: 4012,
+	maxX: 4013,
+	minY: 2711,
+	maxY: 2712
 };
 
-var x, y, outFile, cmd;
-var n, s, e, w;
+
+var x, y;
 
 for ( x = tileSet.minX; x <= tileSet.maxX; x++ ) {
 
 	for ( y = tileSet.minY; y <= tileSet.maxY; y++ ) {
 
-		tileArea( x, y, tileSet.zoomMin, tileSet.dtmMaxZoom );
+		tileArea( x, y, tileSet.minZoom, tileSet.dtmMaxZoom );
 
 	}
 

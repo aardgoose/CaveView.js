b1681d40f01679465994ab8d5dc6b622b996e3f9
aardgoose <angus.sawyer@gmail.com>
2018-01-11 12:22:45 +0000

    remove unused OSGB decoder

--- src/js/core/lib.js
+++ src/js/core/lib.js
@@ -29,26 +29,6 @@ function getEnvironmentValue ( item, defaultValue ) {
 
 }
 
-function toOSref ( coordinate ) {
-
-	var easting  = coordinate.x;
-	var northing = coordinate.y;
-
-	var firstLetter = 'STNOH'.charAt( Math.floor( easting / 500000 ) + Math.floor( northing / 500000 ) * 2 );
-
-	var e2 = Math.floor( ( easting  % 500000 ) / 100000 );
-	var n2 = Math.floor( ( northing % 500000 ) / 100000 );
-
-	var secondLetter = 'VWXYZQRSTULMNOPFGHJKABCDE'.charAt( e2 + n2 * 5 );
-
-	var e3 = Math.floor( easting  % 100000 ).toLocaleString( 'en-GB', { minimumIntegerDigits: 6, useGrouping: false } );
-	var n3 = Math.floor( northing % 100000 ).toLocaleString( 'en-GB', { minimumIntegerDigits: 6, useGrouping: false } );
-
-	return firstLetter + secondLetter + ' ' + e3 + ' ' + n3;
-
-}
-
-
 function replaceExtension( fileName, newExtention ) {
 
 	return fileName.split( '.' ).shift() + '.' + newExtention;
@@ -146,6 +126,6 @@ if ( ! String.prototype.repeat ) {
 }
 
 
-export { toOSref, replaceExtension, setEnvironment, getEnvironmentValue };
+export { replaceExtension, setEnvironment, getEnvironmentValue };
 
 // EOF

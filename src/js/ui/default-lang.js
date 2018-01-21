bb228311e110d641eccb5afc3a661473e33e98e1
aardgoose <angus.sawyer@gmail.com>
2018-01-21 13:03:52 +0000

    WIP support UI translations

---
+++ src/js/ui/default-lang.js
@@ -0,0 +1,24 @@
+const lang_en = {
+	settings: {
+		view: {
+			camera: {
+				'orthographic': 'Orthographic',
+				'perspective': 'Perspective'
+			},
+			autorotate: 'Auto Rotate'
+		},
+		visibility: {
+			entrances: 'Entrances',
+			stations : 'Stations',
+			labels: 'Station Labels',
+			walls: 'Walls (LRUD)',
+			scraps: 'Scraps',
+			splays: 'Splay Legs',
+			traces: 'Dye Traces',
+			box: 'Bounding box',
+			hud: 'Indicators'
+		}
+	}
+};
+
+export { lang_en };

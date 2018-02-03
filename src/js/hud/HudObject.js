5a334e60f727f71f80dd5e8005a89c49285b7da6
aardgoose <angus.sawyer@gmail.com>
2018-02-03 18:42:25 +0000

    use Scale to simplify HUD

--- src/js/hud/HudObject.js
+++ src/js/hud/HudObject.js
@@ -1,40 +1,12 @@
 
-function HudObject () {}
+const HudObject = {
 
-HudObject.stdWidth  = 40;
-HudObject.stdMargin = 5;
+	stdWidth: 40,
+	stdMargin: 5,
 
-HudObject.atlasSpec = {
-	color: '#ffffff',
-	font: 'normal helvetica,sans-serif'
-};
-
-HudObject.prototype.removeDomObjects = function () {
-
-	var obj;
-
-	for ( var i = 0, l = this.domObjects.length; i < l; i++ ) {
-
-		obj = this.domObjects[ i ];
-
-		obj.parentElement.removeChild( obj );
-
-	}
-
-	this.domObjects = [];
-
-};
-
-HudObject.prototype.setVisibility = function ( visible ) {
-
-	this.visible = visible;
-
-	const style = ( visible ? 'block' : 'none' );
-
-	for ( var i = 0, l = this.domObjects.length; i < l; i++ ) {
-
-		this.domObjects[ i ].style.display = style;
-
+	atlasSpec: {
+		color: '#ffffff',
+		font: 'normal helvetica,sans-serif'
 	}
 
 };

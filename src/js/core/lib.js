f77b19b91916f5db097d916e4a9204aec1058362
aardgoose <angus.sawyer@gmail.com>
2018-02-05 10:44:41 +0000

    move defaultTheme to separate file

--- src/js/core/lib.js
+++ src/js/core/lib.js
@@ -2,46 +2,10 @@
 import { Color, FileLoader, EventDispatcher } from '../Three';
 import { x18n } from './x18n';
 import { lang_en } from './default-lang';
+import { defaultTheme } from './defaultTheme';
 
 const environment = new Map();
 const themeColors = new Map();
-
-const defaultTheme = {
-	background: 0x000000,
-	hud: {
-		progress: 0x00ff00,
-		bezel: 0x888888,
-		scale: {
-			bar1: 0xffffff,
-			bar2: 0xff0000,
-		},
-		compass: {
-			top1: 0xb03a14,
-			top2: 0x1ab4e5,
-			bottom1: 0x581d0a,
-			bottom2: 0x0c536a
-		},
-		ahi: {
-			sky: 0x106f8d,
-			earth: 0x802100,
-			bar: 0xffff00,
-			marks: 0xffffff
-		},
-	},
-	box: {
-		bounding: 0xffffff,
-		select: 0x0000ff,
-		highlight: 0xff0000
-	},
-	shading: {
-		single: 0xffffff
-	},
-	popup: {
-		text: 0xffffff,
-		border: 0xffffff,
-		background: 0x222222
-	}
-};
 
 // setup default language
 

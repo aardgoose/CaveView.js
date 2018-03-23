0e77d58a6c0b2e38ec9c7e1d9781727a7c9a980e
aardgoose <angus.sawyer@gmail.com>
2018-03-23 11:33:24 +0000

    remove commonJS from main  build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
 
 function glsl () {
 	return {
@@ -24,29 +22,7 @@ export default {
 		format: 'umd'
 	},
 	plugins: [
-		glsl(),
-
-		nodeResolve({
-			jsnext: true,
-			main: true
-		}),
-
-		commonjs({
-			// non-CommonJS modules will be ignored, but you can also
-			// specifically include/exclude files
-			include: 'node_modules/**',  // Default: undefined
-			//exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
-			// these values can also be regular expressions
-			// include: /node_modules/
-
-			// if false then skip sourceMap generation for CommonJS modules
-			sourceMap: false,  // Default: true
-
-			// explicitly specify unresolvable named exports
-			// (see below for more details)
-			//namedExports: { './node_modules/alpha-shape/alpha.js': [ 'alphaShape' ] },  // Default: undefined
-
-		})
+		glsl()
 	]
 };
 

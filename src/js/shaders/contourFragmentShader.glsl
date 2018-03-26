47d6d687f453c59c2d36183c62ca78727e326c8d
aardgoose <angus.sawyer@gmail.com>
2018-03-26 21:55:34 +0100

    WIP contour shading, add second line colour, track datum shift accurately

--- src/js/shaders/contourFragmentShader.glsl
+++ src/js/shaders/contourFragmentShader.glsl
@@ -1,5 +1,6 @@
 
 uniform vec3 contourColor;
+uniform vec3 contourColor10;
 uniform float contourInterval;
 
 varying vec3 vPosition;
@@ -7,11 +8,14 @@ varying vec3 vPosition;
 void main() {
 
 	float f  = fract ( vPosition.z / contourInterval );
+	float f10  = fract ( ( vPosition.z + 10.0 ) / ( contourInterval * 10.0 ) );
+
 	float df = fwidth( vPosition.z / contourInterval );
 
+	float contourColourSelection = step( 0.90, f10 );
 	float c = 1.0 - smoothstep( df * 1.0, df * 2.0, f );
 
-	gl_FragColor = vec4( c * contourColor, c );
+	gl_FragColor = vec4( c * mix( contourColor, contourColor10, contourColourSelection ), c );
 
 }
 

23196e7c54d35e8fc4d27a30a84539812dbdc050
aardgoose <angus.sawyer@gmail.com>
2018-03-27 11:39:46 +0100

    adjust contour Shading to use correct positions

--- src/js/shaders/contourFragmentShader.glsl
+++ src/js/shaders/contourFragmentShader.glsl
@@ -3,19 +3,21 @@ uniform vec3 contourColor;
 uniform vec3 contourColor10;
 uniform float contourInterval;
 
-varying vec3 vPosition;
+varying float vPositionZ;
 
 void main() {
 
-	float f  = fract ( vPosition.z / contourInterval );
-	float f10  = fract ( ( vPosition.z + 10.0 ) / ( contourInterval * 10.0 ) );
+	float f  = fract ( vPositionZ / contourInterval );
+	float f10  = fract ( ( vPositionZ ) / ( contourInterval * 10.0 ) );
 
-	float df = fwidth( vPosition.z / contourInterval );
+	float df = fwidth( vPositionZ / contourInterval );
 
 	float contourColourSelection = step( 0.90, f10 );
 	float c = 1.0 - smoothstep( df * 1.0, df * 2.0, f );
 
-	gl_FragColor = vec4( c * mix( contourColor, contourColor10, contourColourSelection ), c );
+	vec3 finalColour = mix( contourColor, contourColor10, contourColourSelection );
+
+	gl_FragColor = vec4( c * finalColour, c );
 
 }
 

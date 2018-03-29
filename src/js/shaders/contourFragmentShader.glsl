c842904c21c9cc2a947c1d3968229efe1b4a8189
aardgoose <angus.sawyer@gmail.com>
2018-03-29 21:47:52 +0100

    basic static lighting

--- src/js/shaders/contourFragmentShader.glsl
+++ src/js/shaders/contourFragmentShader.glsl
@@ -2,32 +2,26 @@
 uniform vec3 contourColor;
 uniform vec3 contourColor10;
 uniform float contourInterval;
-uniform vec3 baseColor;
 uniform float opacity;
-uniform vec3 uLight;
 
+varying vec3 vBaseColor;
 varying float vPositionZ;
-varying vec3 vNormal;
-varying vec3 lNormal;
 
 void main() {
 
 	float f  = fract ( vPositionZ / contourInterval );
-	float f10  = fract ( ( vPositionZ ) / ( contourInterval * 10.0 ) );
+	float f10  = fract ( vPositionZ / ( contourInterval * 10.0 ) );
 
 	float df = fwidth( vPositionZ / contourInterval );
 
-	float contourColourSelection = step( 0.90, f10 );
+	float contourColorSelection = step( 0.90, f10 );
 	float c = 1.0 - smoothstep( df * 1.0, df * 2.0, f );
 
-	vec4 finalColour = vec4( mix( contourColor, contourColor10, contourColourSelection ), 1.0 );
+	vec4 finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );
 
-	float nDot = dot( normalize( vNormal ), uLight );
-	float light = 0.5 * ( nDot + 1.0 );
+	vec4 baseColorAlpha = vec4( vBaseColor, opacity );
 
-	vec4 baseColourAlpha = vec4( baseColor * light, opacity );
-
-	gl_FragColor = mix( baseColourAlpha, finalColour, c );
+	gl_FragColor = mix( baseColorAlpha, finalColor, c );
 
 }
 

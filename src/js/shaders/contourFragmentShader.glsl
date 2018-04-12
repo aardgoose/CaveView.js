ba2bfd47586ecfdb12fa48633d6d3ac23cff92f1
aardgoose <angus.sawyer@gmail.com>
2018-04-12 19:45:09 +0100

    remove redundant calc

--- src/js/shaders/contourFragmentShader.glsl
+++ src/js/shaders/contourFragmentShader.glsl
@@ -16,13 +16,13 @@ void main() {
 	float df = fwidth( vPositionZ / contourInterval );
 
 	float contourColorSelection = step( 0.90, f10 );
-	float c = 1.0 - smoothstep( df * 1.0, df * 2.0, f );
+	float c = smoothstep( df * 1.0, df * 2.0, f );
 
 	vec4 finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );
 
 	vec4 baseColorAlpha = vec4( baseColor, opacity );
 
-	gl_FragColor = mix( baseColorAlpha, finalColor, c ) * vDotNL;
+	gl_FragColor = mix( finalColor, baseColorAlpha, c ) * vDotNL;
 
 }
 

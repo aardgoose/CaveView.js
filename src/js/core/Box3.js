972f6ab2f53916e95fd4c7bcd4b1148aa8799642
aardgoose <angus.sawyer@gmail.com>
2018-04-12 19:45:37 +0100

    remove redundant calc

--- src/js/core/Box3.js
+++ src/js/core/Box3.js
@@ -14,7 +14,7 @@ function Box3Helper( box3, color ) {
 	if ( color === undefined ) color = 0xffff00;
 
 	const indices = new Uint16Array( [ 0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7 ] );
-	const positions = new Float32Array( 8 * 3 );
+	const positions = new Float32Array( 24 );
 
 	const geometry = new BufferGeometry();
 

91847fe4f18cdeb86f3ee5a1da2c3a69f2259d8e
aardgoose <angus.sawyer@gmail.com>
2018-04-14 21:12:58 +0100

    add coverage to UK and UK local overlays

--- build/CaveView/lib/BarmasterProvider.js
+++ build/CaveView/lib/BarmasterProvider.js
@@ -7,6 +7,13 @@ function BarmasterProvider () {
 BarmasterProvider.prototype.minZoom = 12;
 BarmasterProvider.prototype.maxZoom = 15;
 
+BarmasterProvider.prototype.coverage = {
+	minX: -206138,
+	minY: 7035233,
+	maxX: -190472,
+	maxY: 7046226
+};
+
 BarmasterProvider.prototype.getUrl = function ( x, y, z ) {
 
 	return 'overlays/barmaster/' + z + '/' + x + '/' + y + '.png';

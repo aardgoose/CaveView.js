94fa08ee31a74f86e66182d4733e3f2c082ff4be
aardgoose <angus.sawyer@gmail.com>
2018-08-01 12:50:04 +0100

    remove old code

--- src/js/viewer/StationPopup.js
+++ src/js/viewer/StationPopup.js
@@ -55,7 +55,6 @@ function StationPopup ( container, station, survey, depth, formatter, showDistan
 
 	} else {
 
-		// this.addLine( 'x: ' + Math.round( position.x ) + ' m' ).addLine( 'y: ' + Math.round( position.y ) + ' m' ).addLine( 'z: ' + position.z + ' m' );
 		this.addLine( 'x: ' + Math.round( position.x ) + ' m, y: ' + Math.round( position.y ) + ' m' ).addLine( 'z: ' + position.z + ' m' );
 
 		if ( depth !== null ) this.addLine( 'depth from surface: ' + Math.round( depth ) + ' m' );

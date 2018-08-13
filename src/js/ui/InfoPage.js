05302ba3495f0069532b10366cec6f485896a15a
aardgoose <angus.sawyer@gmail.com>
2018-08-13 17:18:20 +0100

    refactor UI pages.

---
+++ src/js/ui/InfoPage.js
@@ -0,0 +1,35 @@
+import { VERSION, LEG_CAVE } from '../core/constants';
+
+import { Page } from './Page';
+import { Viewer } from '../viewer/Viewer';
+
+function InfoPage () {
+
+	Page.call( this, 'icon_info', 'info' );
+
+	this.addHeader( 'header' );
+
+	this.addHeader( 'stats.header' );
+
+	const stats = Viewer.getLegStats ( LEG_CAVE );
+
+	this.addLine( this.i18n( 'stats.legs' ) + ': ' + stats.legCount );
+	this.addLine( this.i18n( 'stats.totalLength' ) + ': ' + stats.legLength.toFixed( 2 ) + '\u202fm' );
+	this.addLine( this.i18n( 'stats.minLength' ) + ': ' + stats.minLegLength.toFixed( 2 ) + '\u202fm' );
+	this.addLine( this.i18n( 'stats.maxLength' ) + ': ' + stats.maxLegLength.toFixed( 2 ) + '\u202fm' );
+
+	this.addHeader( 'CaveView v' + VERSION + '.' );
+	this.addText( 'A WebGL 3d cave viewer for Survex (.3d) and Therion (.lox) models.' );
+
+	this.addText( 'For more information see: ' );
+	this.addLink( 'https://aardgoose.github.io/CaveView.js/', 'CaveView on GitHub' );
+	this.addText( '© Angus Sawyer, 2018' );
+
+}
+
+InfoPage.prototype = Object.create( Page.prototype );
+
+export { InfoPage };
+
+
+// EOF
---
+++ src/js/ui/SettingsPage.js
@@ -0,0 +1,98 @@
+import {
+	CAMERA_ORTHOGRAPHIC, CAMERA_PERSPECTIVE,
+	SHADING_CURSOR, SHADING_DEPTH, SHADING_HEIGHT, SHADING_INCLINATION, SHADING_LENGTH,
+	SHADING_SINGLE, SHADING_SURVEY, SHADING_PATH, SHADING_DEPTH_CURSOR, SHADING_DISTANCE,
+	/* SHADING_BECK, */ VIEW_NONE, VIEW_PLAN, VIEW_ELEVATION_N, VIEW_ELEVATION_S, VIEW_ELEVATION_E, VIEW_ELEVATION_W
+} from '../core/constants';
+
+import { Page } from './Page';
+import { Viewer } from '../viewer/Viewer';
+
+const legShadingModes = {
+	'shading.height':        SHADING_HEIGHT,
+	'shading.length':        SHADING_LENGTH,
+	'shading.inclination':   SHADING_INCLINATION,
+	'shading.height_cursor': SHADING_CURSOR,
+	'shading.fixed':         SHADING_SINGLE,
+	'shading.survey':        SHADING_SURVEY,
+	'shading.route':         SHADING_PATH,
+	'shading.distance':      SHADING_DISTANCE
+//	'shading.back':          SHADING_BECK
+};
+
+const cameraViews = {
+	'view.viewpoints.none':        VIEW_NONE,
+	'view.viewpoints.plan':        VIEW_PLAN,
+	'view.viewpoints.elevation_n': VIEW_ELEVATION_N,
+	'view.viewpoints.elevation_s': VIEW_ELEVATION_S,
+	'view.viewpoints.elevation_e': VIEW_ELEVATION_E,
+	'view.viewpoints.elevation_w': VIEW_ELEVATION_W
+};
+
+const cameraModes = {
+	'view.camera.orthographic': CAMERA_ORTHOGRAPHIC,
+	'view.camera.perspective':  CAMERA_PERSPECTIVE
+};
+
+function SettingsPage ( fileSelector ) {
+
+	Page.call( this, 'icon_settings', 'settings' );
+
+	const legShadingModesActive = Object.assign( {}, legShadingModes );
+
+	if ( Viewer.hasRealTerrain ) {
+
+		legShadingModesActive[ 'shading.depth' ] = SHADING_DEPTH;
+		legShadingModesActive[ 'shading.depth_cursor' ] = SHADING_DEPTH_CURSOR;
+
+	}
+
+	this.addHeader( 'survey.header' );
+
+	if ( fileSelector.fileCount > 1 ) {
+
+		this.addSelect( 'survey.caption', fileSelector.fileList, fileSelector, 'file' );
+
+	} else {
+
+		console.log( 'single file' );
+
+	}
+
+	this.addHeader( 'view.header' );
+
+	this.addSelect( 'view.camera.caption', cameraModes, Viewer, 'cameraType' );
+	this.addSelect( 'view.viewpoints.caption', cameraViews, Viewer, 'view' );
+
+	this.addRange( 'view.vertical_scaling', Viewer, 'zScale' );
+
+	this.addCheckbox( 'view.autorotate', Viewer, 'autoRotate' );
+
+	this.addRange( 'view.rotation_speed', Viewer, 'autoRotateSpeed' );
+
+	this.addHeader( 'shading.header' );
+
+	this.addSelect( 'shading.caption', legShadingModesActive, Viewer, 'shadingMode' );
+
+	this.addHeader( 'visibility.header' );
+
+	if ( Viewer.hasEntrances     ) this.addCheckbox( 'visibility.entrances', Viewer, 'entrances' );
+	if ( Viewer.hasStations      ) this.addCheckbox( 'visibility.stations', Viewer, 'stations' );
+	if ( Viewer.hasStationLabels ) this.addCheckbox( 'visibility.labels', Viewer, 'stationLabels' );
+	if ( Viewer.hasSplays        ) this.addCheckbox( 'visibility.splays', Viewer, 'splays' );
+	if ( Viewer.hasWalls         ) this.addCheckbox( 'visibility.walls', Viewer, 'walls' );
+	if ( Viewer.hasScraps        ) this.addCheckbox( 'visibility.scraps', Viewer, 'scraps' );
+	if ( Viewer.hasTraces        ) this.addCheckbox( 'visibility.traces', Viewer, 'traces' );
+
+	this.addCheckbox( 'visibility.fog', Viewer, 'fog' );
+	this.addCheckbox( 'visibility.hud', Viewer, 'HUD' );
+	this.addCheckbox( 'visibility.box', Viewer, 'box' );
+
+}
+
+SettingsPage.prototype = Object.create( Page.prototype );
+
+export { SettingsPage };
+
+
+// EOF

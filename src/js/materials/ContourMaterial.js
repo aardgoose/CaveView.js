4189e83542b455be8ca33ba8619e8f6db7397239
aardgoose <angus.sawyer@gmail.com>
2018-09-11 15:30:44 +0100

    rework custom surface shaders to use MeshLambert + onBeforeCompile()

--- src/js/materials/ContourMaterial.js
+++ src/js/materials/ContourMaterial.js
@@ -1,68 +1,72 @@
 
-import { Shaders } from '../shaders/Shaders';
-import { ShaderMaterial } from '../Three';
+import { MeshLambertMaterial } from '../Three';
 import { Cfg } from '../core/lib';
 
-function ContourMaterial ( survey, viewer ) {
+const fragment_pars = [
+	'uniform vec3 contourColor;',
+	'uniform vec3 contourColor10;',
+	'uniform float contourInterval;',
+	'uniform vec3 baseColor;',
+	'varying float vPositionZ;',
+].join( '\n' );
+
+const fragment_color = [
+	'float f = fract( vPositionZ / contourInterval );',
+	'float f10 = fract( vPositionZ / ( contourInterval * 10.0 ) );',
+	'float df = fwidth( vPositionZ / contourInterval );',
+	'float contourColorSelection = step( 0.90, f10 );',
+	'float c = smoothstep( df * 1.0, df * 2.0, f );',
+	'vec4 finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );',
+	'vec4 baseColorAlpha = vec4( baseColor, opacity );',
+	'diffuseColor = mix( finalColor, baseColorAlpha, c );'
+].join( '\n' );
+
+function ContourMaterial ( survey ) {
+
+	MeshLambertMaterial.call( this );
 
 	this.baseAdjust = survey.offsets.z;
 
 	const terrain = survey.terrain;
 	const zAdjust = this.baseAdjust + terrain.activeDatumShift;
 
-	ShaderMaterial.call( this, {
-		vertexShader:    Shaders.contourVertexShader,
-		fragmentShader:  Shaders.contourFragmentShader,
-		depthWrite:      false,
-		type:            'CV.ContourMaterial',
-		uniforms: {
-			uLight:          { value: viewer.surfaceLightDirection },
+
+	this.transparent = true;
+	this.extensions = { derivatives: true };
+
+	this.onBeforeCompile = function ( shader ) {
+
+		Object.assign( shader.uniforms, {
 			zAdjust:         { value: zAdjust },
 			contourInterval: { value: Cfg.themeValue( 'shading.contours.interval' ) },
 			contourColor:    { value: Cfg.themeColor( 'shading.contours.line' ) },
 			contourColor10:  { value: Cfg.themeColor( 'shading.contours.line10' ) },
-			baseColor:       { value: Cfg.themeColor( 'shading.contours.base' ) },
-			opacity:         { value: 0.5 }
-		}
-	} );
+			baseColor:       { value: Cfg.themeColor( 'shading.contours.base' ) }
+		} );
 
-	this.transparent = true;
+		var vertexShader = shader.vertexShader
+			.replace( '#include <common>', '$&\nuniform float zAdjust;\nvarying float vPositionZ;\n' )
+			.replace( 'include <begin_vertex>', '$&\nvPositionZ = position.z + zAdjust;\n' );
 
-	this.extensions.derivatives = true;
+		var fragmentShader = shader.fragmentShader
+			.replace( '#include <common>', '$&\n' + fragment_pars + '\n' )
+			.replace( '#include <color_fragment>', fragment_color );
 
-	Object.defineProperty( this, 'opacity', {
-		writeable: true,
-		get: function () { return this.uniforms.opacity.value; },
-		set: function ( value ) { this.uniforms.opacity.value = value; }
-	} );
+		shader.vertexShader = vertexShader;
+		shader.fragmentShader = fragmentShader;
 
-	this.callback = this.lightingChanged.bind( this );
-
-	viewer.addEventListener( 'lightingChange', this.callback );
+	};
 
 	return this;
 
 }
 
-ContourMaterial.prototype = Object.create( ShaderMaterial.prototype );
+ContourMaterial.prototype = Object.create( MeshLambertMaterial.prototype );
 
-ContourMaterial.prototype.lightingChanged = function ( event ) {
-
-	this.uniforms.uLight.value = event.position;
-
-};
 
 ContourMaterial.prototype.setDatumShift = function ( shift ) {
 
 	this.uniforms.zAdjust.value = this.baseAdjust + shift;
-
-};
-
-ContourMaterial.prototype.dispose = function ( viewer ) {
-
-	viewer.removeEventListener( 'lightingChange', this.callback );
-
-	ShaderMaterial.prototype.dispose.call( this );
 
 };
 

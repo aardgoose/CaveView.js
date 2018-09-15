a81bb8db18af213bb48765138b106a5095aef5aa
aardgoose <angus.sawyer@gmail.com>
2018-09-15 21:56:24 +0100

    WIP annotation

--- src/js/ui/AnnotatePanel.js
+++ src/js/ui/AnnotatePanel.js
@@ -1,6 +1,7 @@
 
 import { Viewer } from '../viewer/Viewer';
 import { Panel } from './Panel';
+import { Annotations } from '../viewer/Annotations';
 
 
 const annotators = [
@@ -23,10 +24,7 @@ function AnnotatePanel ( page ) {
 
 	function _onSelect ( event ) {
 
-		self.onShow();
-
 		const annotationInfo = event.annotationInfo;
-
 		var annotation = annotationInfo.annotation;
 
 		if ( annotation === undefined ) {
@@ -35,17 +33,79 @@ function AnnotatePanel ( page ) {
 				type: 'none'
 			};
 
+			self.onShow();
+			self.addDynamic( page.addLine( 'Station: ' + annotationInfo.name ) );
+			self.addDynamic( page.addSelect( 'annotate.type', annotators, annotation, 'type' ) );
+
+			self.addDynamic( page.addButton( 'annotate.select_type', function () {
+
+				annotationInfo.annotation = annotation;
+
+				if ( annotation.type !== 'none' ) {
+
+					annotationInfo.annotation = annotation;
+					_onType( event );
+
+				}
+
+			} ) );
+
+		} else {
+
+			_onType( event );
+
 		}
 
-		console.log( 'annot: ', annotation );
+	}
 
-		self.addDynamic( page.addLine( annotationInfo.name ) );
-		self.addDynamic( page.addSelect( 'annotate.type', annotators, annotation, 'type' ) );
+	function _onType ( event ) {
 
-		self.addDynamic( page.addButton( 'annotate.set', function () {
-			console.log( 'set annotation: ', annotation );
-			event.add( annotation );
-			self.onShow();
+		const annotationInfo = event.annotationInfo;
+		var annotation = annotationInfo.annotation;
+
+		self.onShow();
+		self.addDynamic( page.addLine( 'Station: ' + annotationInfo.name ) );
+		self.addDynamic( page.addLine( 'Type: ' + annotation.type ) );
+
+		const annotator = Annotations.getAnnotator( annotation.type );
+
+		annotator.parameters.forEach( function ( parameter ) {
+
+			const value = annotation[ parameter.name ] || '';
+
+			self.addDynamic( page.addTextBox( parameter.caption, value, function ( v ) {
+				parameter.getResult = v;
+			} ) );
+
+		} );
+
+		self.addDynamic( page.addButton( 'annotate.save', function () {
+
+			let errors = 0;
+
+			annotator.parameters.forEach( function ( parameter ) {
+
+				const result = parameter.getResult();
+
+				if ( parameter.valid( result ) ) {
+
+					annotation[ parameter.name ] = result;
+
+				} else {
+
+					errors++;
+
+				}
+
+			} );
+
+			if ( errors === 0 ) {
+
+				event.add( annotation );
+				self.onShow();
+
+			}
+
 		} ) );
 
 	}

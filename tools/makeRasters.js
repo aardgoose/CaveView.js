07346a9775fc3b696759a349dd939c9d49f5eb7c
aardgoose <angus.sawyer@gmail.com>
2018-11-07 12:03:43 +0000

    WIP improve tile generation

--- tools/makeRasters.js
+++ tools/makeRasters.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var fs = require( 'fs' );
 const { execSync } = require( 'child_process' );
 
 const halfMapExtent = 6378137 * Math.PI; // from EPSG:3875 definition
@@ -11,67 +12,60 @@ function runCmd( cmd ) {
 
 }
 
-/*
-var mapSet = 'Austria';
-var sourceRaster = 'dhm_lamb_10m';
+function createRasters( tileSet ) {
 
-var tileSet = {
-	title: 'Loser',
-	dtmMaxZoom: 17,
-	maxZoom: 18,
-	minZoom: 13,
-	divisions: 128,
-	directory: '',
-	subdirectory: 'Loser',
-	dtmScale: 64,
-	minX: 4409,
-	maxX: 4410,
-	minY: 2857,
-	maxY: 2859
-};
-*/
+	var mapSet = 'TEST';
+	var sourceRaster = 'dummy';
 
-var mapSet = 'Matienzo';
-var sourceRaster = 'SOURCE';
+	var n, s, e, w, zoom;
 
-var tileSet = {
-	title: 'Matienzo',
-	dtmMaxZoom: 16,
-	maxZoom: 18,
-	minZoom: 12,
-	divisions: 128,
-	directory: '',
-	subdirectory: 'Matienzo',
-	dtmScale: 64,
-	minX: 2005,
-	maxX: 2008,
-	minY: 1498,
-	maxY: 1500
-};
+	var maxTileWidth = halfMapExtent / Math.pow( 2, tileSet.minZoom - 1 );
 
-var n, s, e, w, zoom;
+	for ( zoom = tileSet.minZoom; zoom <= tileSet.dtmMaxZoom; zoom++ ) {
 
-var maxTileWidth = halfMapExtent / Math.pow( 2, tileSet.minZoom - 1 );
+		var tileWidth = halfMapExtent / Math.pow( 2, zoom - 1 );
+		var resolution = tileWidth / tileSet.divisions; // note: tile area extended by resolution/2 all round giving 129 sample row & columns
+		var offset = resolution / 2;
 
-for ( zoom = tileSet.minZoom; zoom <= tileSet.dtmMaxZoom; zoom++ ) {
+		n =   halfMapExtent - tileSet.minY * maxTileWidth + offset; //
+		s =   halfMapExtent - ( tileSet.maxY + 1) * maxTileWidth - offset;
 
-	var tileWidth = halfMapExtent / Math.pow( 2, zoom - 1 );
-	var resolution = tileWidth / tileSet.divisions; // note: tile area extended by resolution/2 all round giving 129 sample row & columns
-	var offset = resolution / 2;
+		e = - halfMapExtent + ( tileSet.maxX + 1 ) * maxTileWidth + offset;
+		w = - halfMapExtent + tileSet.minX * maxTileWidth - offset;
 
-	n =   halfMapExtent - tileSet.minY * maxTileWidth + offset; //
-	s =   halfMapExtent - ( tileSet.maxY + 1) * maxTileWidth - offset;
+		var tempFile = 'DTM' + zoom + 'M@' + mapSet;
 
-	e = - halfMapExtent + ( tileSet.maxX + 1 ) * maxTileWidth + offset;
-	w = - halfMapExtent + tileSet.minX * maxTileWidth - offset;
+		// define region
 
-	runCmd( 'g.region n=' + n + ' s=' + s + ' w=' +  w + ' e=' + e + ' nsres=' + resolution + ' ewres=' + resolution );
+		runCmd( 'g.region n=' + n + ' s=' + s + ' w=' +  w + ' e=' + e + ' nsres=' + resolution + ' ewres=' + resolution );
 
-	runCmd( 'r.resamp.interp --o input=' +  sourceRaster + '@' + mapSet + ' output=DTM' + zoom + 'M@' + mapSet );
+		// produce downres of source
 
-	// scale by 64 to increase resolution as a 16b integer (smaller files and type usable by OpenGL)
-	runCmd( 'r.mapcalc --o "DTM' + zoom + 'X=round(DTM' + zoom + 'M@' + mapSet + ' * 64)"' );
+		runCmd( 'r.resamp.stats --verbose --o input=' + sourceRaster + '@' + mapSet + ' output=' + tempFile );
+
+		// scale by 64 to increase resolution as a 16b integer (smaller files and type usable by OpenGL)
+		runCmd( 'r.mapcalc --o "DTM' + zoom + 'X=round(' + tempFile + ' * 64)"' );
+
+		runCmd( 'g.remove -f type=raster name=' + tempFile );
+
+	}
 
 }
+
+const setName = 'Pokljuka';
+
+var tileSetsText = fs.readFileSync( 'tileSets.json' );
+var tileSets = JSON.parse( tileSetsText );
+
+var tileSet = tileSets.find(  function ( e ) { return e.title === setName ;} );
+
+if ( tileSet !== undefined ) {
+
+	console.log( 'Creating rasters' );
+
+	createRasters( tileSet );
+
+}
+
 
 // EOF

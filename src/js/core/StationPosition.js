992ad6864cff5d4717a743bd97fb529cf024f272
aardgoose <angus.sawyer@gmail.com>
2018-12-04 11:03:50 +0000

    handle XSECTS that end on a splay

--- src/js/core/StationPosition.js
+++ src/js/core/StationPosition.js
@@ -14,6 +14,7 @@ StationPosition.scaleFactor = 1;
 Object.assign( StationPosition.prototype, Vector3.prototype );
 
 StationPosition.prototype.connections = 0;
+StationPosition.prototype.splays = 0;
 
 StationPosition.prototype.correctedDistanceTo = function ( v ) {
 

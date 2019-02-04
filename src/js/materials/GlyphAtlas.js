5cd53a546dc1b69f0f141676fa6a931c9783c234
aardgoose <angus.sawyer@gmail.com>
2019-02-04 20:38:36 +0000

    add dynamic addition to glyph atlases

--- src/js/materials/GlyphAtlas.js
+++ src/js/materials/GlyphAtlas.js
@@ -1,4 +1,4 @@
-import { CanvasTexture } from '../Three';
+import { CanvasTexture, LinearFilter } from '../Three';
 
 function GlyphAtlas ( glyphAtlasSpec ) {
 
@@ -8,10 +8,17 @@ function GlyphAtlas ( glyphAtlasSpec ) {
 	const divisions = atlasSize / cellSize;
 
 	const canvas = document.createElement( 'canvas' );
-	const map = {};
 
 	const glyphs = '\u202f\u00B0\u2610 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789%,.-_/()[]\'"';
 	const glyphCount = glyphs.length;
+
+	this.fontSize = fontSize;
+	this.cellScale = cellSize / atlasSize;
+	this.cellSize = cellSize;
+	this.glyphCount = glyphCount;
+	this.divisions = divisions;
+
+	this.map = {};
 
 	if ( glyphCount > divisions * divisions ) {
 
@@ -22,7 +29,7 @@ function GlyphAtlas ( glyphAtlasSpec ) {
 
 	if ( ! canvas ) console.error( 'creating canvas for glyph atlas failed' );
 
-	canvas.width  = atlasSize;
+	canvas.width = atlasSize;
 	canvas.height = atlasSize;
 
 	const ctx = canvas.getContext( '2d' );
@@ -31,38 +38,47 @@ function GlyphAtlas ( glyphAtlasSpec ) {
 
 	// set background
 	ctx.fillStyle = glyphAtlasSpec.background || 'rgba( 0, 0, 0, 0 )';
-
 	ctx.fillRect( 0, 0, atlasSize, atlasSize );
 
-	// populate with glyphs
-
+	// set up text settings
 	ctx.textAlign = 'left';
-	ctx.font = fontSize + 'px ' + glyphAtlasSpec.font;
+	ctx.font = this.fontSize + 'px ' + glyphAtlasSpec.font;
 	ctx.fillStyle = glyphAtlasSpec.color || '#ffffff';
+
+	this.ctx = ctx;
 
 	for ( var i = 0; i < glyphCount; i++ ) {
 
-		const glyph = glyphs.charAt( i );
-		const glyphWidth = ctx.measureText( glyph ).width / cellSize;
-
-		const row = Math.floor( i / divisions ) + 1;
-		const column = i % divisions;
-
-		map[ glyph ] = { row: ( divisions - row ) / divisions, column: column / divisions, width: glyphWidth };
-
-		ctx.fillText( glyph, cellSize * column, cellSize * row - 8 );
+		this.addGlyphToCanvas( glyphs.charAt( i ), i );
 
 	}
 
 	this.texture = new CanvasTexture( canvas );
-
-	this.texture.onUpdate = function _dropCanvas ( texture ) { texture.image = null; };
-
-	this.map = map;
-	this.cellScale = cellSize / atlasSize;
-	this.cellSize = cellSize;
+	this.texture.minFilter = LinearFilter;
+	this.generateMipmaps = false;
 
 }
+
+GlyphAtlas.prototype.addGlyphToCanvas = function ( glyph, i ) {
+
+	const divisions = this.divisions;
+	const ctx = this.ctx;
+	const cellSize = this.cellSize;
+
+	const glyphWidth = ctx.measureText( glyph ).width / cellSize;
+
+	const row = Math.floor( i / divisions ) + 1;
+	const column = i % divisions;
+
+	const glyphData =  { row: ( divisions - row ) / divisions, column: column / divisions, width: glyphWidth };
+
+	this.map[ glyph ] = glyphData;
+
+	ctx.fillText( glyph, cellSize * column, cellSize * row - 8 );
+
+	return glyphData;
+
+};
 
 GlyphAtlas.prototype.getTexture = function () {
 
@@ -76,8 +92,16 @@ GlyphAtlas.prototype.getGlyph = function ( glyph ) {
 
 	if ( glyphData === undefined ) {
 
-		console.warn( 'unavailable glyph [' + glyph + ']', glyph.codePointAt() );
-		glyphData = this.map[ '\u2610' ];  // substitute empty box
+		if ( this.glyphCount + 1 > this.divisions * this.divisions ) {
+
+			console.warn( 'too many glyphs for atlas when adding [' + glyph + ']' );
+			return;
+
+		}
+
+		glyphData = this.addGlyphToCanvas( glyph, this.glyphCount++ );
+
+		this.texture.needsUpdate = true;
 
 	}
 

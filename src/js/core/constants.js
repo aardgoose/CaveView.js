aae66d9bdc1968846aa60eac0d27b229fa480d1a
aardgoose <angus.sawyer@gmail.com>
2019-06-19 12:08:01 +0100

    add new layer type

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -66,6 +66,7 @@ export const FACE_SCRAPS           = 12;
 export const LABEL_STATION         = 13;
 export const SURVEY_WARNINGS       = 14;
 export const LABEL_STATION_COMMENT = 15;
+export const CLUSTER_MARKERS       = 16;
 
 export const WALL_OVAL             = 1; // based on Therion .lox types
 export const WALL_SQUARE           = 2;

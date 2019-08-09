0a0debe525931644e0dc82272abd0f6913396e13
aardgoose <angus.sawyer@gmail.com>
2019-08-09 17:11:12 +0100

    refactor common uniform setting

--- src/js/materials/TerrainOverlayMaterial.js
+++ src/js/materials/TerrainOverlayMaterial.js
@@ -1,13 +1,12 @@
 
 import { MeshLambertMaterial } from '../Three';
-import { Cfg } from '../core/lib';
-import { Vector2 } from 'three';
+import { CommonTerrainUniforms } from './CommonTerrainUniforms';
 
 const fragment_pars = [
 	'uniform float scale;',
 	'uniform float accuracy;',
 	'uniform vec2 target;',
-	'uniform vec3 contourColor;',
+	'uniform vec3 ringColor;',
 	'varying vec2 vPosition;'
 ].join( '\n' );
 
@@ -15,7 +14,7 @@ const fragment_color = [
 	'float targetDistance = distance( target, vPosition );',
 	'float f = abs( targetDistance - accuracy ) * scale;',
 	'float c = smoothstep( 1.0, 6.0, f );',
-	'diffuseColor = mix( vec4( contourColor, 0.7 ), diffuseColor, c );'
+	'diffuseColor = mix( vec4( ringColor, 0.7 ), diffuseColor, c );'
 ].join( '\n' );
 
 function TerrainOverlayMaterial ( parameters ) {
@@ -28,13 +27,7 @@ function TerrainOverlayMaterial ( parameters ) {
 	this.onBeforeCompile = function ( shader ) {
 
 		// some uniforms shared by all material instances
-
-		Object.assign( shader.uniforms, {
-			scale: TerrainOverlayMaterial.scale,
-			accuracy: TerrainOverlayMaterial.accuracy,
-			target: TerrainOverlayMaterial.target,
-			contourColor: { value: Cfg.themeColor( 'shading.contours.line' ) },
-		} );
+		Object.assign( shader.uniforms, CommonTerrainUniforms );
 
 		var vertexShader = shader.vertexShader
 			.replace( '#include <common>', '$&\nvarying vec2 vPosition;\n' )
@@ -52,28 +45,6 @@ function TerrainOverlayMaterial ( parameters ) {
 	return this;
 
 }
-
-TerrainOverlayMaterial.scale = { value: 1.0 };
-TerrainOverlayMaterial.accuracy = { value: 0.0 };
-TerrainOverlayMaterial.target = { value: new Vector2() };
-
-TerrainOverlayMaterial.setScale = function ( scale ) {
-
-	TerrainOverlayMaterial.scale.value = scale;
-
-};
-
-TerrainOverlayMaterial.setAccuracy = function ( accuracy ) {
-
-	TerrainOverlayMaterial.accuracy.value = accuracy;
-
-};
-
-TerrainOverlayMaterial.setTarget = function ( target ) {
-
-	TerrainOverlayMaterial.target.value.copy( target );
-
-};
 
 TerrainOverlayMaterial.prototype = Object.create( MeshLambertMaterial.prototype );
 

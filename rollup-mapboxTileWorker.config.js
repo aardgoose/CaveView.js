54aaefe8b4fd2644df580461c7dcd3ed48dc8b0c
aardgoose <angus.sawyer@gmail.com>
2019-08-20 15:56:51 +0100

    WIP mapbox DTM

---
+++ rollup-mapboxTileWorker.config.js
@@ -0,0 +1,51 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+//import builtins from 'rollup-plugin-node-builtins';
+//import globals from 'rollup-plugin-node-globals';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+
+export default {
+	treeshake: true,
+	input: 'src/js/workers/mapboxTileWorker.js',
+	output: {
+		file: 'build/CaveView/js/workers/mapboxTileWorker.js',
+		format: 'umd'
+	},
+	plugins: [
+//		builtins(),
+//		globals(),
+		json({
+			// All JSON files will be parsed by default,
+			// but you can also specifically include/exclude files
+			include: [ 'node_modules/**', 'src/js/**' ],
+			exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
+
+			// for tree-shaking, properties will be declared as
+			// variables, using either `var` or `const`
+			preferConst: true, // Default: false
+
+			// specify indentation for the generated default export —
+			// defaults to '\t'
+			indent: '  '
+		}),
+		nodeResolve( {} ),
+
+		commonjs({
+			// non-CommonJS modules will be ignored, but you can also
+			// specifically include/exclude files
+			include: 'node_modules/**',  // Default: undefined
+
+			// search for files other than .js files (must already
+			// be transpiled by a previous plugin!)
+			extensions: [ '.js' ],  // Default: [ '.js' ]
+			// if true then uses of `global` won't be dealt with by this plugin
+			ignoreGlobal: false,  // Default: false
+			// if false then skip sourceMap generation for CommonJS modules
+			sourceMap: false,  // Default: true,
+			namedExports: {
+				'node_modules/utf8/utf8.js': [ 'decode', 'encode' ]
+			}
+		})
+
+	]
+};
---
+++ src/js/workers/mapboxTileWorker.js
@@ -0,0 +1,166 @@
+
+import 'three/src/polyfills';
+import { HeightMapboxLoader } from '../loaders/HeightMapboxLoader';
+import { TerrainTileGeometry } from '../terrain/TerrainTileGeometry';
+import { decode } from 'fast-png';
+
+const halfMapExtent = 6378137 * Math.PI; // from EPSG:3875 definition
+var tileSpec;
+
+onmessage = onMessage;
+
+function onMessage ( event ) {
+console.log( decode );
+	tileSpec = event.data;
+
+	const tileSet = tileSpec.tileSet;
+
+	if ( tileSet.isFlat ) {
+
+		mapLoaded( null );
+
+	} else {
+
+		new HeightMapboxLoader( tileSpec, mapLoaded, mapError ).load();
+
+	}
+
+}
+
+function mapLoaded ( data ) {
+
+	var tileSet = tileSpec.tileSet;
+	console.log( 'oooo', data );
+
+	var terrainData;
+
+	terrainData = new Uint16Array( data );
+
+	switch ( tileSpec.request ) {
+
+	case 'tile':
+
+		loadTile( terrainData );
+		break;
+
+	case 'height':
+
+		getHeight( terrainData );
+		break;
+
+	default:
+
+		console.log( 'webTileWorker: unknown request type' );
+		mapError();
+
+	}
+
+}
+
+function getHeight ( terrainData ) {
+
+	const offsets = tileSpec.dataOffsets;
+	const points = tileSpec.points;
+
+	const pointCount = offsets.length;
+
+	var i;
+
+	for ( i = 0; i < pointCount; i++ ) {
+
+		points[ i ].z = terrainData[ offsets[ i ] ] / tileSpec.tileSet.dtmScale;
+
+	}
+
+	postMessage( { status: 'ok', points: points} );
+
+}
+
+function loadTile ( terrainData ) {
+
+	// clip height map data
+
+	const clip      = tileSpec.clip;
+	const offsets   = tileSpec.offsets;
+	const tileSet   = tileSpec.tileSet;
+	const divisions = tileSpec.divisions;
+
+	const xDivisions = divisions - clip.left - clip.right;
+	const yDivisions = divisions - clip.top - clip.bottom;
+
+	const resolution = tileSpec.resolution;
+
+	const xTileWidth = resolution * xDivisions;
+	const yTileWidth = resolution * yDivisions;
+
+	clip.terrainHeight = divisions;
+	clip.terrainWidth  = divisions;
+
+	// offsets to translate tile to correct position relative to model centre
+
+	offsets.x = resolution * ( tileSpec.x * divisions + clip.left ) - halfMapExtent - offsets.x;
+	offsets.y = halfMapExtent - resolution * ( tileSpec.y * divisions + clip.top ) - offsets.y;
+
+	var terrainTile;
+
+	if ( tileSet.isFlat ) {
+
+		terrainTile = new FlatTileGeometry( xTileWidth, yTileWidth, clip, offsets, tileSpec.flatZ );
+
+	} else {
+
+		terrainTile = new TerrainTileGeometry( xTileWidth, yTileWidth, xDivisions, yDivisions, terrainData, tileSet.dtmScale, clip, offsets );
+
+	}
+
+	// avoid calculating bounding box in main thread.
+	// however it isn't preserved in json serialisation.
+
+	const bb = terrainTile.boundingBox;
+
+	const boundingBox = {
+
+		min: {
+			x: bb.min.x,
+			y: bb.min.y,
+			z: bb.min.z
+		},
+
+		max: {
+			x: bb.max.x,
+			y: bb.max.y,
+			z: bb.max.z
+		}
+
+	};
+
+	// support transferable objects where possible
+
+	const indexBuffer = terrainTile.index.array.buffer;
+	const attributes = {};
+	const transferable = [];
+
+	const srcAttributes = terrainTile.attributes;
+
+	for ( var attributeName in srcAttributes ) {
+
+		const attribute = srcAttributes[ attributeName ];
+		const arrayBuffer = attribute.array.buffer;
+
+		attributes[ attributeName ] = { array: arrayBuffer, itemSize: attribute.itemSize };
+
+		transferable.push( arrayBuffer );
+
+	}
+
+	postMessage( { status: 'ok', index: indexBuffer, attributes: attributes, boundingBox: boundingBox }, transferable );
+
+}
+
+function mapError () {
+
+	postMessage( { status: 'nomap' } );
+
+}
+
+// EOF

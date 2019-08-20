33b70ad8cc0d05a540b852130e2b933f359960cb
aardgoose <angus.sawyer@gmail.com>
2019-08-20 11:54:09 +0100

    smooth both edges of contour lines

--- src/js/materials/ContourMaterial.js
+++ src/js/materials/ContourMaterial.js
@@ -13,10 +13,11 @@ const fragment_pars = [
 
 const fragment_color = [
 	'float f = fract( vPositionZ / contourInterval );',
+	'if ( f > 0.5 ) f = 1.0 - f;',
 	'float f10 = fract( vPositionZ / ( contourInterval * 10.0 ) );',
 	'float df = fwidth( vPositionZ / contourInterval );',
 	'float contourColorSelection = step( 0.90, f10 );',
-	'float c = smoothstep( df * 1.0, df * 2.0, f );',
+	'float c = smoothstep( df * 0.5, df * 1.0, f );',
 	'vec4 finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );',
 	'vec4 baseColorAlpha = vec4( baseColor, opacity );',
 	'diffuseColor = mix( finalColor, baseColorAlpha, c );'

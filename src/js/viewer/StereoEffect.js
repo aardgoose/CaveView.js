05962c7cbde16a0dfbdf4061794686dd4befbfc2
aardgoose <angus.sawyer@gmail.com>
2019-08-31 15:53:26 +0100

    correct doc tags

--- src/js/viewer/StereoEffect.js
+++ src/js/viewer/StereoEffect.js
@@ -1,8 +1,8 @@
 /**
  * @author alteredq / http://alteredqualia.com/
- * @authod mrdoob / http://mrdoob.com/
- * @authod arodic / http://aleksandarrodic.com/
- * @authod fonserbc / http://fonserbc.github.io/
+ * @author mrdoob / http://mrdoob.com/
+ * @author arodic / http://aleksandarrodic.com/
+ * @author fonserbc / http://fonserbc.github.io/
 */
 
 import { StereoCamera } from '../Three';

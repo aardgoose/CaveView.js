b034e98816a4022432d2e84bde8353b098ee2673
aardgoose <angus.sawyer@gmail.com>
2020-05-17 15:20:09 +0100

    remove old code remains and surplus header

--- src/js/ui/EditPage.js
+++ src/js/ui/EditPage.js
@@ -1,14 +1,12 @@
-import { SHADING_PATH, MOUSE_MODE_ROUTE_EDIT, MOUSE_MODE_TRACE_EDIT, MOUSE_MODE_NORMAL, /* MOUSE_MODE_ENTRANCES, MOUSE_MODE_ANNOTATE */ } from '../core/constants';
+import { SHADING_PATH, MOUSE_MODE_ROUTE_EDIT, MOUSE_MODE_TRACE_EDIT, MOUSE_MODE_NORMAL, /* MOUSE_MODE_ENTRANCES */ } from '../core/constants';
 
 import { Page } from './Page';
 import { RoutePanel } from './RoutePanel';
 import { TracePanel } from './TracePanel';
-//import { AnnotatePanel } from './AnnotatePanel';
 //import { EntrancePanel } from './EntrancePanel';
 
 const mode = {
 	'modes.none': MOUSE_MODE_NORMAL,
-	//	'modes.annotate': MOUSE_MODE_ANNOTATE,
 	// 'modes.entrances': MOUSE_MODE_ENTRANCES,
 	'modes.route': MOUSE_MODE_ROUTE_EDIT,
 	'modes.trace': MOUSE_MODE_TRACE_EDIT
@@ -25,12 +23,9 @@ function EditPage ( frame, viewer, fileSelector ) {
 
 	var initialState;
 
-	// var annotatePanel = null;
 	var routePanel = null;
 	var tracePanel = null;
 	// var entrancePanel = null;
-
-	this.addHeader( 'header' );
 
 	this.addSelect( 'mode', mode, viewer, 'editMode' );
 
@@ -73,14 +68,6 @@ function EditPage ( frame, viewer, fileSelector ) {
 
 				break;
 
-			case MOUSE_MODE_ANNOTATE:
-
-				if ( annotatePanel === null ) annotatePanel = new AnnotatePanel( self, viewer );
-
-				newState.stations = true;
-				newState.annotations = true;
-
-				break;
 			*/
 
 			}
@@ -89,7 +76,6 @@ function EditPage ( frame, viewer, fileSelector ) {
 
 			frame.setControlsVisibility( intro, viewer.editMode === MOUSE_MODE_NORMAL );
 
-			// if ( annotatePanel !== null ) annotatePanel.setVisibility( viewer.editMode === MOUSE_MODE_ANNOTATE );
 			// if ( entrancePanel !== null ) entrancePanel.setVisibility( viewer.editMode === MOUSE_MODE_ENTRANCES );
 			if ( routePanel !== null ) routePanel.setVisibility( viewer.editMode === MOUSE_MODE_ROUTE_EDIT );
 			if ( tracePanel !== null ) tracePanel.setVisibility( viewer.editMode === MOUSE_MODE_TRACE_EDIT );
@@ -103,7 +89,6 @@ function EditPage ( frame, viewer, fileSelector ) {
 		// save initial view settings
 
 		initialState = {
-			// annotations: viewer.annotations,
 			shadingMode: viewer.shadingMode,
 			// entrances: viewer.entrances,
 			stations: viewer.stations,

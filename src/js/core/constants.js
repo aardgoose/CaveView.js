91aa491c30b800af415862221adbff6333f198fb
aardgoose <angus.sawyer@gmail.com>
2020-05-18 16:04:59 +0100

    remove redundant constant

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -60,7 +60,6 @@ export const FEATURE_ENTRANCES     = 6;
 export const FEATURE_TERRAIN       = 7;
 export const FEATURE_STATIONS      = 8;
 export const FEATURE_TRACES        = 9;
-export const FEATURE_ANNOTATIONS   = 10;
 export const FACE_WALLS            = 11;
 export const FACE_SCRAPS           = 12;
 export const LABEL_STATION         = 13;

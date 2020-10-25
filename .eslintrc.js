c9f1dc038b9ce9c556d8570f0fea5d1b03f35c0c
aardgoose <angus.sawyer@gmail.com>
2020-10-25 15:58:38 +0000

    lint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "worker": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": "off",
-        "no-trailing-spaces": "error"
-    }
+	"env": {
+		"browser": true,
+		"worker": true,
+		"es6": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-console": "off",
+		"no-trailing-spaces": "error",
+		"prefer-const": ["error", {
+			"destructuring": "any",
+			"ignoreReadBeforeAssign": false
+		}]
+	}
 };

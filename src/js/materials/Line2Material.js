1a78aba7725366fe4d89a2864eae61274a6494b9
aardgoose <angus.sawyer@gmail.com>
2021-02-04 12:14:22 +0000

    add basic line2 material (wip)

---
+++ src/js/materials/Line2Material.js
@@ -0,0 +1,215 @@
+import {
+	ShaderMaterial,
+	UniformsLib,
+	UniformsUtils,
+	Vector2
+} from '../three';
+import { Shaders } from '../shaders/Shaders';
+
+const uniforms = UniformsUtils.merge( [
+	UniformsLib.common,
+	UniformsLib.fog,
+	{
+		linewidth: { value: 1 },
+		resolution: { value: new Vector2( 1, 1 ) },
+		dashScale: { value: 1 },
+		dashSize: { value: 1 },
+		dashOffset: { value: 0 },
+		gapSize: { value: 1 }, // todo FIX - maybe change to totalSize
+		opacity: { value: 1 }
+	}
+] );
+
+var Line2Material = function ( ctx, params ) {
+
+	const defines = {};
+
+	defines.CV_BASIC = true;
+
+	ShaderMaterial.call( this, {
+
+		type: 'LineMaterial',
+
+		uniforms: Object.assign(
+			UniformsUtils.clone( uniforms ),
+			ctx.materials.commonUniforms
+		),
+
+		vertexShader: Shaders.lineVertexShader,
+		fragmentShader: Shaders.lineFragmentShader,
+
+		clipping: true, // required for clipping support
+		defines: defines
+	} );
+
+	this.dashed = false;
+
+	Object.defineProperties( this, {
+
+		color: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.diffuse.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.diffuse.value = value;
+
+			}
+
+		},
+
+		linewidth: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.linewidth.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.linewidth.value = value;
+
+			}
+
+		},
+
+		dashScale: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.dashScale.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.dashScale.value = value;
+
+			}
+
+		},
+
+		dashSize: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.dashSize.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.dashSize.value = value;
+
+			}
+
+		},
+
+		dashOffset: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.dashOffset.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.dashOffset.value = value;
+
+			}
+
+		},
+
+		gapSize: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.gapSize.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.gapSize.value = value;
+
+			}
+
+		},
+
+		opacity: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.opacity.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.opacity.value = value;
+
+			}
+
+		},
+
+		resolution: {
+
+			enumerable: true,
+
+			get: function () {
+
+				return this.uniforms.resolution.value;
+
+			},
+
+			set: function ( value ) {
+
+				this.uniforms.resolution.value.copy( value );
+
+			}
+
+		}
+
+	} );
+
+	this.setValues( params );
+
+	this.resolution = new Vector2( ctx.container.clientWidth, ctx.container.clientHeight );
+
+	ctx.viewer.addEventListener( 'resized', ( e ) => {
+
+		const lineScale = e.lineScale ? e.lineScale : 1;
+
+		this.resolution = new Vector2( e.width, e.height );
+		this.linewidth = Math.max( 1, Math.floor( e.width / 1000 ) * lineScale );
+
+	} );
+
+};
+
+Line2Material.prototype = Object.create( ShaderMaterial.prototype );
+Line2Material.prototype.constructor = Line2Material;
+
+Line2Material.prototype.isLineMaterial = true;
+
+export { Line2Material };

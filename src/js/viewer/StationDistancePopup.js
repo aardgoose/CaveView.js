50157d5852b94a30823adecbd9e38120fb88257b
aardgoose <angus.sawyer@email.com>
2022-05-03 15:14:29 +0100

    add connecting line

--- src/js/viewer/StationDistancePopup.js
+++ src/js/viewer/StationDistancePopup.js
@@ -1,5 +1,7 @@
 import { Vector3 } from '../Three';
 import { CanvasPopup } from './CanvasPopup';
+import { LineSegmentsGeometry } from '../core/LineSegmentsGeometry';
+import { LineSegments2 } from '../core/LineSegments2';
 
 const __v1 = new Vector3();
 const __v2 = new Vector3();
@@ -22,6 +24,23 @@ class StationDistancePopup extends CanvasPopup {
 		this.addValue( 'distance', p1.distanceTo( p2 ) );
 
 		this.finish( endStation );
+
+		const geometry = new LineSegmentsGeometry();
+
+		geometry.setPositions( [
+			startStation.x, startStation.y, startStation.z,
+			endStation.x, endStation.y, endStation.z
+		] );
+
+		this.line = new LineSegments2( geometry, ctx.materials.getLine2Material() );
+		survey.addStatic( this.line );
+
+	}
+
+	close () {
+
+		super.close();
+		this.line.removeFromParent();
 
 	}
 

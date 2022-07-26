428aaaec569274dcfb28e042e4e185c5a6dece42
aardgoose <angus.sawyer@email.com>
2022-07-26 09:11:51 +0100

    improve countour shader

--- src/js/materials/shaders/chunks/contour_fragment.glsl
+++ src/js/materials/shaders/chunks/contour_fragment.glsl
@@ -1,7 +1,20 @@
-float f = fract( vPositionZ / contourInterval );if ( f > 0.5 ) f = 1.0 - f;
-float f10 = fract( vPositionZ / ( contourInterval * 10.0 ) );
-float df = fwidth( vPositionZ / contourInterval );
-float contourColorSelection = step( 0.90, f10 );
+
+float zLine = vPositionZ / contourInterval;
+
+float f = fract( zLine );
+float f10 = fract( zLine / 10.0 );
+
+float df = fwidth( zLine );
+
+if ( f > 0.5 ) {
+
+    f = 1.0 - f;
+    f10 = 1.0 - f10;
+
+}
+
+float contourColorSelection = step( 0.91, f10 );
+
 float c = smoothstep( df * 0.5, df * 1.0, f );
 
 vec4 finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );

34ba65104a8c59dca5273a5f5129d0c4b5d7fa24
aardgoose <angus.sawyer@email.com>
2022-11-01 10:22:45 +0000

    use bilinear interpolation in texture lookups

--- src/js/core/TextureLookup.js
+++ src/js/core/TextureLookup.js
@@ -8,7 +8,7 @@ class TextureLookup {
 	base = null;
 	buffer = null;
 	range = null;
-	tranform = new Matrix3();
+	transform = new Matrix3();
 	width = null;
 
 	constructor ( renderer, renderTarget, boundingBox ) {
@@ -28,8 +28,8 @@ class TextureLookup {
 		const base = boundingBox.min;
 		const range = boundingBox.getSize( new Vector3() );
 
-		this.tranform.scale( width / range.x, height/ range.y, 1 );
-		this.tranform.multiply( new Matrix3().translate( - base.x, - base.y ) );
+		this.transform.scale( width / range.x, height/ range.y, 1 );
+		this.transform.multiply( new Matrix3().translate( - base.x, - base.y ) );
 
 		this.base = base;
 		this.range = range;
@@ -37,15 +37,42 @@ class TextureLookup {
 
 	}
 
-	lookup ( point ) {
+	subLookup ( x, y ) {
 
-		const v = __v.copy( point).setZ( 1 ).applyMatrix3( this.tranform ).round();
-
-		const offset = ( v.x + v.y * this.width ) * 4;
+		const offset = ( x + y * this.width ) * 4;
 
 		// convert to survey units and return
 
 		return unpackRGBA( this.buffer.subarray( offset, offset + 4 ) );
+
+	}
+
+	lookup ( point ) {
+
+		const v = __v.copy( point ).setZ( 1 ).applyMatrix3( this.transform );
+
+		// bilinear interpolation
+
+		const xMin = Math.floor( v.x );
+		const yMin = Math.floor( v.y );
+		const xMax = Math.ceil( v.x );
+		const yMax = Math.ceil( v.y );
+
+		const x = v.x - xMin;
+		const y = v.y - yMin;
+
+		const l1 = this.subLookup( xMin, yMin );
+		const l2 = this.subLookup( xMax, yMin );
+		const l3 = this.subLookup( xMin, yMax );
+		const l4 = this.subLookup( xMax, yMax );
+
+		const l0 =
+			l1 * ( 1 - x) * ( 1 - y) +
+			l2 * x * ( 1 - y ) +
+			l3 * ( 1 - x ) * y +
+			l4 * x * y;
+
+		return l0;
 
 	}
 

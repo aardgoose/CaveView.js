1c162da596c47767142371173b2b9f15a5203624
aardgoose <angus.sawyer@email.com>
2022-12-26 15:36:32 +0000

    add generic worker handler for loading models in worker threads

---
+++ src/js/loaders/WorkerHandler.js
@@ -0,0 +1,78 @@
+class WorkerHandler {
+
+	constructor( file, script ) {
+
+		this.file = file;
+		this.script = script;
+
+	}
+
+	load ( loadingContext, progress, model ) {
+
+		const worker = new Worker( this.script );
+
+		return new Promise( ( resolve, reject ) => {
+
+			worker.onmessage = event => {
+
+				const data = event.data;
+
+				if ( data.status == 'progress' ) {
+
+					progress( data );
+					return;
+
+				}
+
+				model.limits.copy( data.boundingBox );
+				model.models.push( data );
+
+				resolve( data.status == 'ok' )
+
+				worker.terminate();
+
+			}
+
+			worker.postMessage( { file: this.file, loadingContext: loadingContext } );
+
+		} );
+
+	}
+
+	parse ( model, progress, prefix ) {
+
+		model.setCRS( null );
+
+		const worker = new Worker( this.script );
+
+		return new Promise( ( resolve, reject ) => {
+
+			worker.onmessage = event => {
+
+				const data = event.data;
+
+				if ( data.status == 'progress' ) {
+
+					progress( data );
+					return;
+
+				}
+
+				model.limits.copy( data.boundingBox );
+				model.models.push( data );
+
+				resolve( data.status == 'ok' )
+
+				worker.terminate();
+
+			}
+
+			worker.postMessage( prefix + this.fileName );
+
+		} );
+
+	}
+
+}
+
+export { WorkerHandler };

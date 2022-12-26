8759c636cceae9436fc450ecceb098e002598f38
aardgoose <angus.sawyer@email.com>
2022-12-26 15:38:05 +0000

    remove old code

--- src/js/loaders/WorkerHandler.js
+++ src/js/loaders/WorkerHandler.js
@@ -39,40 +39,6 @@ class WorkerHandler {
 
 	}
 
-	parse ( model, progress, prefix ) {
-
-		model.setCRS( null );
-
-		const worker = new Worker( this.script );
-
-		return new Promise( ( resolve, reject ) => {
-
-			worker.onmessage = event => {
-
-				const data = event.data;
-
-				if ( data.status == 'progress' ) {
-
-					progress( data );
-					return;
-
-				}
-
-				model.limits.copy( data.boundingBox );
-				model.models.push( data );
-
-				resolve( data.status == 'ok' )
-
-				worker.terminate();
-
-			}
-
-			worker.postMessage( prefix + this.fileName );
-
-		} );
-
-	}
-
 }
 
 export { WorkerHandler };

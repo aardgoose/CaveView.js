325552e644d4a3da400b9469449c05d157fa8eaf
aardgoose <angus.sawyer@email.com>
2023-06-10 23:05:10 +0100

    wip node materials

--- src/js/nodeMaterials/ContourMaterial.js
+++ src/js/nodeMaterials/ContourMaterial.js
@@ -1,4 +1,4 @@
-import { MeshPhongNodeMaterial, fract, fwidth, step, expression, cond, mix, smoothstep, uniform, vec4, positionLocal, materialOpacity } from '../../../node_modules/three/examples/jsm/nodes/Nodes';
+import { MeshPhongNodeMaterial, fract, fwidth, step, cond, mix, smoothstep, uniform, vec4, positionLocal, materialOpacity } from '../../../node_modules/three/examples/jsm/nodes/Nodes';
 
 class ContourMaterial extends MeshPhongNodeMaterial {
 
@@ -32,7 +32,7 @@ class ContourMaterial extends MeshPhongNodeMaterial {
 		cond( f.greaterThan( 0.5 ), f = f.oneMinus() );
 		cond( f.greaterThan( 0.5 ), f10 = f10.oneMinus() );
 
-		const contourColorSelection = step( 0.91, f10 );
+		const contourColorSelection = step( 0.81, f10 );
 
 		const c = smoothstep( df.mul( 0.5 ), df, f );
 		const finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );
@@ -40,52 +40,6 @@ class ContourMaterial extends MeshPhongNodeMaterial {
 		this.opacityNode = mix( 1.0, materialOpacity, c );
 		this.colorNode = mix( finalColor, baseColor, c );
 
-
-		/*
-
-
-float zLine = vPositionZ / contourInterval;
-
-float f = fract( zLine );
-float f10 = fract( zLine / 10.0 );
-
-float df = fwidth( zLine );
-
-if ( f > 0.5 ) {
-
-    f = 1.0 - f;
-    f10 = 1.0 - f10;
-
-}
-
-float contourColorSelection = step( 0.91, f10 );
-
-float c = smoothstep( df * 0.5, df * 1.0, f );
-
-vec4 finalColor = vec4( mix( contourColor, contourColor10, contourColorSelection ), 1.0 );
-vec4 baseColorAlpha = vec4( baseColor, opacity );
-
-diffuseColor = mix( finalColor, baseColorAlpha, c );
-*/
-
-
-		/*
-		this.onBeforeCompile = function ( shader ) {
-
-			this.commonBeforeCompile( ctx, shader );
-
-			Object.assign( shader.uniforms, {
-				zOffset = uniform(  survey.offsets.z },
-				contourInterval = uniform(  cfg.themeValue( 'shading.contours.interval' ) },
-				contourColor = uniform(  cfg.themeColor( 'shading.contours.line' ) },
-				contourColor10 = uniform(  cfg.themeColor( 'shading.contours.line10' ) },
-				baseColor = uniform(  cfg.themeColor( 'shading.contours.base' ) }
-			}, materials.uniforms.commonDepth );
-
-			this.editShaderInclude( shader, 'contour' );
-
-		};
-*/
 	}
 
 }

6bbf3b667ddf9502416458d7a71162daed74326c
aardgoose <angus.sawyer@email.com>
2023-06-23 15:17:31 +0100

    WIP DepthMap

--- src/js/materials/DepthMapMaterial.js
+++ src/js/materials/DepthMapMaterial.js
@@ -1,33 +1,42 @@
 import { NoBlending } from '../Three';
-import { NodeMaterial, saturate, uniform, varying, vec4, positionGeometry } from '../Nodes';
+import { NodeMaterial, saturate, color, uniform, varying, vec4, positionGeometry } from '../Nodes';
 
 class DepthMapMaterial extends NodeMaterial {
+
+	name = 'DepthMapMaterial';
 
 	constructor ( terrain ) {
 
 		super();
 
-//		this.isTest = true;
-
 		const boundingBox = terrain.boundingBox;
 
 		const minHeight = boundingBox.min.z;
 		const maxHeight = boundingBox.max.z;
-		console.log( minHeight, maxHeight );
-		const minZ = uniform( minHeight, 'float' );
-		const scaleZ = uniform( 1 / ( maxHeight - minHeight ), 'float' );
+
+		const minZ = uniform( minHeight );
+		const scaleZ = uniform( 1 / ( maxHeight - minHeight ) );
 		const vHeight = varying( saturate( positionGeometry.z.sub( minZ ).mul( scaleZ ) ) );
 
-		this.colorNode = vec4( vHeight, 0, 1.0, 1 );
-//		this.colorNode = vec4( 0.5, 0, 1.0, 1 );
+		this.colorNode = vec4( vHeight, 0, 0, 1 );
+
 		this.blending = NoBlending;
 		this.normals = false;
+		this.depthTest = false;
 
 	}
 
 	constructOutput( /* builder, outgoingLight, opacity  */) {
 
 		return this.colorNode;
+
+	}
+
+	constructDiffuseColor( /* builder */  ) {}
+
+	customProgramCacheKey () {
+
+		'CV:DepthMapMaterial';
 
 	}
 

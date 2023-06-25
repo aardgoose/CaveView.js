a1c1829373ff60ea666ed595511db06d25623b93
aardgoose <angus.sawyer@email.com>
2023-06-25 14:16:36 +0100

    WIP conversion of anaglyph effect

--- src/js/viewer/AnaglyphEffect.js
+++ src/js/viewer/AnaglyphEffect.js
@@ -7,42 +7,19 @@
 
 import {
 	LinearFilter,
-	Matrix3,
 	Mesh,
 	NearestFilter,
 	OrthographicCamera,
 	PlaneGeometry,
 	RGBAFormat,
 	Scene,
-	ShaderMaterial,
 	StereoCamera,
 	WebGLRenderTarget
 } from '../Three';
+import { AnaglyphMaterial } from '../materials/AnaglyphMaterial';
 
-//import { Shaders } from '../materials/shaders/Shaders';
 
 function AnaglyphEffect ( renderer, width, height ) {
-
-	// Matrices generated with angler.js https://github.com/tschw/angler.js/
-	// (in column-major element order, as accepted by WebGL)
-
-	this.colorMatrixLeft = new Matrix3().fromArray( [
-
-		1.0671679973602295, 	-0.0016435992438346148,		 0.0001777536963345483, // r out
-		-0.028107794001698494,	-0.00019593400065787137,	-0.0002875397040043026, // g out
-		-0.04279090091586113,	 0.000015809757314855233,	-0.00024287120322696865 // b out
-
-	] );
-
-	//	red						green 						blue  						in
-
-	this.colorMatrixRight = new Matrix3().fromArray( [
-
-		-0.0355340838432312,	-0.06440307199954987,		 0.018319187685847282,	// r out
-		-0.10269022732973099,	 0.8079727292060852,		-0.04835830628871918,	// g out
-		0.0001224992738571018,	-0.009558862075209618,		 0.567823588848114		// b out
-
-	] );
 
 	const _camera = new OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
 
@@ -63,30 +40,18 @@ function AnaglyphEffect ( renderer, width, height ) {
 	const _renderTargetL = new WebGLRenderTarget( width * pixelRatio, height * pixelRatio, _params );
 	const _renderTargetR = new WebGLRenderTarget( width * pixelRatio, height * pixelRatio, _params );
 
-	const _material = new ShaderMaterial( {
-
-		uniforms: {
-
-			'mapLeft': { value: _renderTargetL.texture },
-			'mapRight': { value: _renderTargetR.texture },
-
-			'colorMatrixLeft': { value: this.colorMatrixLeft },
-			'colorMatrixRight': { value: this.colorMatrixRight }
-
-		},
-
-//		vertexShader: Shaders.anaglyphVertexShader,
-//		fragmentShader: Shaders.anaglyphFragmentShader
-
+	const _material = new AnaglyphMaterial( {
+		left: _renderTargetL.texture,
+		right: _renderTargetR.texture
 	} );
 
 	const _mesh = new Mesh( new PlaneGeometry( 2, 2 ), _material );
 	_scene.add( _mesh );
-
+console.log( _mesh );
 	this.setLayers = function ( mask ) {
-
-		_stereo.cameraL.layers.mask = mask;
-		_stereo.cameraR.layers.mask = mask;
+//
+//		_stereo.cameraL.layers.mask = mask;
+//		_stereo.cameraR.layers.mask = mask;
 
 	};
 
@@ -114,7 +79,7 @@ function AnaglyphEffect ( renderer, width, height ) {
 		if ( camera.parent === null ) camera.updateMatrixWorld();
 
 		_stereo.update( camera );
-
+console.log( 'red0');
 		renderer.setRenderTarget( _renderTargetL );
 		renderer.clear();
 		renderer.render( scene, _stereo.cameraL );
@@ -122,9 +87,10 @@ function AnaglyphEffect ( renderer, width, height ) {
 		renderer.setRenderTarget( _renderTargetR );
 		renderer.clear();
 		renderer.render( scene, _stereo.cameraR );
-
+console.log( 'red1' );
 		renderer.setRenderTarget( null );
 		renderer.render( _scene, _camera );
+		console.log( 'red2');
 
 	};
 

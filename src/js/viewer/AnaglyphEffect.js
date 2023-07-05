4186c238ace143911b4bb20e09392f70ab4dfe84
aardgoose <angus.sawyer@email.com>
2023-07-05 16:18:04 +0100

    analglyph node shader

--- src/js/viewer/AnaglyphEffect.js
+++ src/js/viewer/AnaglyphEffect.js
@@ -18,7 +18,6 @@ import {
 } from '../Three';
 import { AnaglyphMaterial } from '../materials/AnaglyphMaterial';
 
-
 function AnaglyphEffect ( renderer, width, height ) {
 
 	const _camera = new OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
@@ -30,15 +29,12 @@ function AnaglyphEffect ( renderer, width, height ) {
 	_stereo.cameraL.layers.mask = 0xFFFFFFFF;
 	_stereo.cameraR.layers.mask = 0xFFFFFFFF;
 
-	const _params = { minFilter: LinearFilter, magFilter: NearestFilter, format: RGBAFormat };
-
-	if ( width === undefined ) width = 512;
-	if ( height === undefined ) height = 512;
+	const _params = { minFilter: LinearFilter, magFilter: NearestFilter, format: RGBAFormat, stencil: true };
 
 	const pixelRatio = renderer.getPixelRatio();
 
-	const _renderTargetL = new WebGLRenderTarget( width * pixelRatio, height * pixelRatio, _params );
-	const _renderTargetR = new WebGLRenderTarget( width * pixelRatio, height * pixelRatio, _params );
+	const _renderTargetL = new WebGLRenderTarget( width * pixelRatio, height * pixelRatio );
+	const _renderTargetR = new WebGLRenderTarget( width * pixelRatio, height * pixelRatio );
 
 	const _material = new AnaglyphMaterial( {
 		left: _renderTargetL.texture,
@@ -47,11 +43,11 @@ function AnaglyphEffect ( renderer, width, height ) {
 
 	const _mesh = new Mesh( new PlaneGeometry( 2, 2 ), _material );
 	_scene.add( _mesh );
-console.log( _mesh );
+
 	this.setLayers = function ( mask ) {
-//
-//		_stereo.cameraL.layers.mask = mask;
-//		_stereo.cameraR.layers.mask = mask;
+
+		_stereo.cameraL.layers.mask = mask;
+		_stereo.cameraR.layers.mask = mask;
 
 	};
 
@@ -72,25 +68,26 @@ console.log( _mesh );
 
 	};
 
-	this.render = function ( scene, camera ) {
+	this.render = async function ( scene, camera ) {
 
 		scene.updateMatrixWorld();
 
 		if ( camera.parent === null ) camera.updateMatrixWorld();
+		_stereo.eyeSep = 2.0;
 
 		_stereo.update( camera );
-console.log( 'red0');
+
 		renderer.setRenderTarget( _renderTargetL );
-		renderer.clear();
 		renderer.render( scene, _stereo.cameraL );
 
 		renderer.setRenderTarget( _renderTargetR );
-		renderer.clear();
 		renderer.render( scene, _stereo.cameraR );
-console.log( 'red1' );
+
 		renderer.setRenderTarget( null );
+//		renderer.clear();
 		renderer.render( _scene, _camera );
 		console.log( 'red2');
+
 
 	};
 

a02eb3633e4bde47cb8d70a515af83d19f1450a2
aardgoose <angus.sawyer@email.com>
2024-04-05 13:19:07 +0100

    use packed float

--- src/js/materials/DepthMapMaterial.js
+++ src/js/materials/DepthMapMaterial.js
@@ -1,5 +1,5 @@
 import { NoBlending } from '../Three';
-import { NodeMaterial, saturate, uniform, varying, vec4, positionGeometry } from '../Nodes';
+import { NodeMaterial, saturate, uniform, varying, tslFn, vec3, vec4, temp, positionGeometry } from '../Nodes';
 
 class DepthMapMaterial extends NodeMaterial {
 
@@ -12,25 +12,34 @@ class DepthMapMaterial extends NodeMaterial {
 		const minHeight = boundingBox.min.z;
 		const maxHeight = boundingBox.max.z;
 
-		const minZ = uniform( minHeight );
-		const scaleZ = uniform( 1 / ( maxHeight - minHeight ) );
-		const vHeight = varying( saturate( positionGeometry.z.sub( minZ ).mul( scaleZ ) ) );
 
-		this.colorNode = vec4( vHeight, 0, 0, 1 );
+		const PackUpscale = 256. / 255.; // fraction -> 0..1 (including 1)
+		const PackFactors = vec3( 256. * 256. * 256., 256. * 256., 256. );
+		const ShiftRight8 = 1. / 256.;
+
+		this.fragmentNode = tslFn( () => {
+
+			const minZ = uniform( minHeight );
+			const scaleZ = uniform( 1 / ( maxHeight - minHeight ) );
+			const vHeight = varying( saturate( positionGeometry.z.sub( minZ ).mul( scaleZ ) ) );
+
+			const r = temp( vec4() ).assign( vHeight.mul( PackFactors ).fract(), vHeight );
+
+			r.subAssign( vec4( 0, r.xyz.mul( ShiftRight8 ) ) ); // tidy overflow
+			r.mulAssign( PackUpscale );
+
+			return r;
+
+		} )();
 
 		this.blending = NoBlending;
 		this.normals = false;
 		this.depthTest = false;
+		this.colorSpaced = false;
+		this.fog = false;
+		this.toneMapped = false;
 
 	}
-
-	constructOutput( /* builder, outgoingLight, opacity  */) {
-
-		return this.colorNode;
-
-	}
-
-	constructDiffuseColor( /* builder */  ) {}
 
 }
 
